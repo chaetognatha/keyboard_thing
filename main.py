ec1453f4e52830b50d845ef01504447f537885aa
StonesRocks <68532873+StonesRocks@users.noreply.github.com>
2022-10-21 15:24:57 +0200

    Added pseudocode

--- main.py
+++ main.py
@@ -14,17 +14,25 @@ Special: use OOP to model the keyboard circuit
 class Keyboard:
     def __init__(self, rows, cols, json_file):
         self.json_file = json_file
-        # import json file as dict
+        # import json file as dict and generate node locations based on the jason file.
         with open(json_file) as f:
             self.json_dict = json.load(f)
 
+
+        # Matrix where rows and cols depend on amount of pins. example 18 pins -> sqrt(18) = 9 -> 9 rows and 9 columns. for uneven amount of pins make columns>rows
+        # Note that these cols and rows are NOT defining the keeb cols and rows, this is specifically for the matrix and will always be as square as possible
         self.rows = rows
         self.cols = cols
         self.n = rows * cols
+
         self.switches = ['SW' + str(i) for i in range(1, self.n+1)]
         self.keyboard = nx.Graph()
         self.keyboard.add_nodes_from(self.switches)
 
+    def keebnodes(self, json_dict):
+        # Process json into nodes
+        # These will become the nodes used to measure fitness.
+        # Testing out with an 8x2 should be sufficient to test out the algorithm, lowest amount of pins is 8 while the regular setup requires 10, highest should be 16.
 
 # define population class
 class Population:
@@ -32,5 +40,30 @@ class Population:
         # size of population
         self.count = count
         # RULES FOR GENERATING INDIVIDUALS
+        # 1. grab copy of switches.
+        # 2. grab copy of keyboard.
+        # 3. start at first node and randomize one of the item in switches list, continue until every node has one switch.
+        # 4. this is now a generated keeb.
+
+        #RULES FOR CROSSOVER
+        # 1. randomize where to split the switches, draw a line between first switch and n switch.
+        # 2. decide which part will be kept and which will mutate(since kept/locked switches will have different locations on each keeb we'll mutate the other parents gene to fit it)
+        # 3. mutate gene to fit the other keyboard.
+        # 3.1. lock switches to nodes that will not mutate/will be kept, if switch x is used on parent1 but not parent2 then parent2 will have an equivalent switch y.
+        # 3.2. grab all unlocked switches from parent1 and place them as close as possible to their node position on parent1 but on parent2, the node on parent2 might be occupied by a locked switch and therefore will look for a closer unlocked switch, this will be the mutation.
+        # 3.3. repeat step 3.2 on parent1 with switches from parent2
+        # 3.4. two new children has been generated.
+
+        #RULES FOR FITNESS
+        # 1. summarize all unique rows and columns, this pin score is more important than the distance score.
+        # 2. sort used switches by row
+        # 3. grab all switches with same row and calculate distance score of each row by shortest distance to connect all switches of same row.
+        # 3.2. note that the distance is based on the nodes and not switch matrix, we also need an algorithm to find shortest path.
+        # 4. do step 2 but with columns instead of rows and add them all together, the smaller the score the better.
+        # 5. do some math where fewer pins used(fewer unique rows and columns) is better fitness as well as lower distance score, note that pin score weights more than distance score, this should avoid the algorithm to apply a unique row and column for each switch(giving it a 0 distance score) when amount of pins>=switches.
+
+
+
+
         # initialize keyboard population
         self.population = [Keyboard(12, 10) for i in range(self.count)]
