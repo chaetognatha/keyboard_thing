8e192b6b1bd9813fea34617c85a8fa1c2d663630
Mattis <mattisknulst@gmail.com>
2022-10-21 07:03:39 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import networkx as nx
+import libpysal as ps
+"""
+Objective: Model a keyboard circuit to determine the optimal circuit design
+Special: use OOP to model the keyboard circuit
+"""
+# define keyboard class
+
+#define population class
