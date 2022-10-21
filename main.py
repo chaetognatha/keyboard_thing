10f94264d1fb5b569ddafc83d5c19f1d55a13352
Mattis <mattisknulst@gmail.com>
2022-10-21 07:24:47 +0200

    json

--- main.py
+++ main.py
@@ -4,23 +4,26 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import networkx as nx
 import libpysal as ps
+import json
 """
 Objective: Model a keyboard circuit to determine the optimal circuit design
 Special: use OOP to model the keyboard circuit
 """
 # define keyboard class
 class Keyboard:
-    def __init__(self, rows, cols):
+    def __init__(self, rows, cols, json_file):
+        self.json_file = json_file
+        # import json file as dict
+        with open(json_file) as f:
+            self.json_dict = json.load(f)
+
         self.rows = rows
         self.cols = cols
         self.n = rows * cols
         self.switches = ['SW' + str(i) for i in range(1, self.n+1)]
         self.keyboard = nx.Graph()
         self.keyboard.add_nodes_from(self.switches)
-        for i in range(1, self.rows+1):
-            for j in range(1, self.cols+1):
-                self.keyboard.nodes['SW' + str((i-1)*self.cols + j)]['coord'] = (i, j)
-        self.end_node = 'SW' + str(self.n)
+
 
 # define population class
 class Population:
