be3bd100c4d59422be2a03f625d3c5500a41c0d9
Mattis <mattisknulst@gmail.com>
2022-10-21 07:06:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,17 @@ Objective: Model a keyboard circuit to determine the optimal circuit design
 Special: use OOP to model the keyboard circuit
 """
 # define keyboard class
+class Keyboard:
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        self.n = rows * cols
+        self.switches = ['SW' + str(i) for i in range(1, self.n+1)]
+        self.keyboard = nx.Graph()
+        self.keyboard.add_nodes_from(self.switches)
+        for i in range(1, self.rows+1):
+            for j in range(1, self.cols+1):
+                self.keyboard.nodes['SW' + str((i-1)*self.cols + j)]['coord'] = (i, j)
+        self.end_node = 'SW' + str(self.n)
 
 #define population class
